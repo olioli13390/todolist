fbf33366462cc2b5775f24c85fa825c0f4e5448e
olioli13390 <olivier001@hotmail.fr>
2025-02-26 11:21:37 +0100

    v1.0

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,37 +1,63 @@
+
 let task = document.querySelector('#task')
 let submit = document.querySelector('#submit')
 let taskList = document.querySelector('#taskList')
 let taskOver = document.querySelector('#taskOver')
 let deleteNav = document.querySelector('#deleteNav')
 let intro = document.querySelector("#intro")
+let alertMessage = document.querySelector(' #alert-message')
 
 
-submit.addEventListener('click', function () {
+submit.addEventListener('click', function (event) {
     event.preventDefault();
-    intro.classList.remove('hidden')
-    let taskText = task.value
-    if (taskText != '') {
+    let taskText = task.value;
+    let existingMessage = alertMessage.querySelector('.message');
+
+    if (taskText === '') {
+        if (!existingMessage) {
+            let message = document.createElement('p');
+            message.innerHTML = `<p class="message">Veuillez entrer une tâche.</p>`;
+            alertMessage.appendChild(message);
+            alertMessage.classList.remove('hidden');
+            submit.classList.add('hidden');
+
+            alertMessage.addEventListener('click', () => {
+                alertMessage.classList.add('hidden');
+                message.remove();
+                submit.classList.remove('hidden');
+            }, { once: true });
+        }
+    } else {
+        if (existingMessage) {
+            existingMessage.remove();
+            alertMessage.classList.add('hidden');
+            submit.classList.remove('hidden');
+        }
+        intro.classList.remove('hidden');
         let li = document.createElement('li');
-        li.innerHTML += `<div> 
-        <img src="./asset/style/Image/point.png" alt="" class="pointer">
-        ${taskText}</div><i class="fas fa-times" id="delete"></i>`;
-        taskList.appendChild(li)
+        li.innerHTML = `<div>
+            <img src="./asset/style/Image/point.png" alt="" class="pointer">
+            ${taskText}</div><i class="fas fa-times" id="delete"></i>`;
+        taskList.appendChild(li);
         deleteLi(li);
-        done(li)
-        task.value = ''
-    };
-
-
-    function deleteLi(li) {
-        let deleteTask = li.querySelector('.fa-times')
-        deleteTask.addEventListener('click', function () {
-            event.stopPropagation()
-            li.remove()
-        })
+        done(li);
+        task.value = '';
     }
-})
 
+
+});
+
+// supprime une tache non réalisée définitvement
+
+function deleteLi(li) {
+    let deleteTask = li.querySelector('.fa-times')
+    deleteTask.addEventListener('click', function () {
+        event.stopPropagation()
+        li.remove()
+    })
+}
 /// Finir une tache en cliquant sur celle-ci
+
 function done(li) {
     let clickCount = 0
     li.addEventListener('click', function () {
@@ -50,6 +76,7 @@ function done(li) {
     });
 }
 
+// fais disparaitre le message pour la liste des taches fini
 function clearing(li) {
     if (taskOver.querySelectorAll(li).length === 0) {
         deleteNav.style.display = "none"
